d95e7a935057991995b2b9d195da390a89b5245c
Jon O'Drobinak <jodrobinak@gmail.com>
2019-04-13 14:06:15 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:lighter,400,extrabold,800" rel="stylesheet">
+    <title>Check Yo' Self</title>
+  </head>
---
+++ to-do-list.js
@@ -0,0 +1,25 @@
+class ToDoList {
+    constructor(id, title, urgent, tasks) {
+        this.id = id;
+        this.title = title;
+        this.urgent = urgent || false;
+        this.tasks = [];
+    }
+    saveToStorage() {
+
+    }
+
+    deleteFromStorage() {
+
+    }
+
+    updateToDo() {
+
+    }
+
+    updateTask() {
+        
+    }
+}
+
+module.exports = ToDoList;
